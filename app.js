93206f42034339037ebfceeab242d0fa27cfa724
Anthony Nguyen <anthonynuge0509@gmail.com>
2022-10-25 14:00:24 -0500

    Create basic math functions

---
+++ app.js
@@ -0,0 +1,30 @@
+// Math Functions for basic math operands=====================================================================
+
+function add(a, b) {
+    return a + b;
+};
+
+function subtract(a, b) {
+    return a - b;
+};
+
+function multiply(a, b) {
+    return a * b;
+};
+
+function divide(a, b) {
+    return a / b;
+};
+
+// operate function takes an operator and 2 numbers and calls above functions on the numbers
+function operate(a, b, operator) {
+    if (operator === "+") {
+        return add(a, b);
+    } else if (operator === "-") {
+        return subtract(a, b);
+    } else if (operator === "*") {
+        return multiply(a, b)
+    } else if (operator === "/") {
+        return divide(a, b);
+    };
+};
