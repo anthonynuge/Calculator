4e30d7b3957f27625d3f9e2fdc44236419b094cf
Anthony Nguyen <anthonynuge0509@gmail.com>
2022-10-25 23:33:09 -0500

    Add button for numbers functionality

--- app.js
+++ app.js
@@ -1,5 +1,14 @@
-// Math Functions for basic math operands=====================================================================
+// important variables ==========================================================
+const calculationDisplay = document.querySelector(".calculationDisplay");
+const lastDisplay = document.querySelector(".lastDisplay");
+const currentDisplay = document.querySelector(".currentDisplay");
+const btns = document.querySelectorAll("button");
+const digits = document.querySelectorAll(".digit");
+const operators = document.querySelectorAll(".operator");
 
+
+
+// Math Functions for basic math operands=====================================================================
 function add(a, b) {
     return a + b;
 };
@@ -16,6 +25,7 @@ function divide(a, b) {
     return a / b;
 };
 
+
 // operate function takes an operator and 2 numbers and calls above functions on the numbers
 function operate(a, b, operator) {
     if (operator === "+") {
@@ -28,3 +38,58 @@ function operate(a, b, operator) {
         return divide(a, b);
     };
 };
+
+// function to display text context ================================
+function displayLast(lastOperation) {
+    const oldLast = document.querySelector(".lastContent");
+    lastDisplay.removeChild(oldLast);
+    const lastOutput = document.createElement('div');
+    lastOutput.classList.add("lastContent");
+    lastOutput.textContent = lastOperation;
+    lastDisplay.appendChild(lastOutput);
+}
+
+function displayCurrent(currentOperation) {
+    const oldCurrent = document.querySelector(".currentContent");
+    currentDisplay.removeChild(oldCurrent);
+    const currentOutput = document.createElement('div');
+    currentOutput.classList.add("currentContent");
+    currentOutput.textContent = currentOperation;
+    currentDisplay.appendChild(currentOutput);
+}
+
+// buttons events
+// btns.forEach((button) => {
+//     button.addEventListener("click", () => {
+//         console.log(typeof(button.value));
+//         console.log(button.value);
+//     });
+// });
+
+let tempMemory = [];
+
+digits.forEach((button) => {
+    button.addEventListener("click", () => {
+        tempMemory.push(button.value);
+        console.log(tempMemory);
+        let strArray = tempMemory.join("");
+        displayCurrent(strArray);
+    });
+}); 
+
+
+operators.forEach((button) => {
+    button.addEventListener("click", () => {
+        let operator = button.value;
+        tempMemory.push(button.value)
+        console.log(operator); 
+        // phase = 1;
+    }); 
+});
+
+//////Main code ====
+// let phase = "p1";
+
+// while (phase === "p1") {
+    
+// }
